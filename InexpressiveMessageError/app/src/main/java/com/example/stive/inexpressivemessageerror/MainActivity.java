8322e6be3371c51b71ad82cdeb1f6eb7e1395c54
MichaelOsorio2017 <ms.osorio@uniandes.edu.co>
2017-12-14 15:59:34 -0500

    cambios

--- InexpressiveMessageError/app/src/main/java/com/example/stive/inexpressivemessageerror/MainActivity.java
+++ InexpressiveMessageError/app/src/main/java/com/example/stive/inexpressivemessageerror/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.stive.inexpressivemessageerror;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch wifi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        wifi = (Switch)findViewById(R.id.switch1);
+
+        Boolean switchStatie = wifi.isChecked();
 
     }
 }
