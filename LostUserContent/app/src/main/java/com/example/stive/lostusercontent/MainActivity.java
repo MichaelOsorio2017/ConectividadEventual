80c9a05d3b9731568904f50ebfff821fc9a8035a
MichaelOsorio2017 <ms.osorio@uniandes.edu.co>
2018-01-06 19:27:22 -0500

    Lost User Content Diseñada

--- LostUserContent/app/src/main/java/com/example/stive/lostusercontent/MainActivity.java
+++ LostUserContent/app/src/main/java/com/example/stive/lostusercontent/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.stive.lostusercontent;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView description;
+    RelativeLayout lostContentDetail;
+    Button btnLostUserContent;
+    Button btnLostUserExample;
+    Button btnTestLostUserContent;
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        description = findViewById(R.id.antipattern);
+        lostContentDetail = findViewById(R.id.lostUserContentDetail);
+        btnLostUserExample = findViewById(R.id.btnExampleLostUserContent);
+        btnTestLostUserContent = findViewById(R.id.btnLivetest);
+        btnLostUserContent = findViewById(R.id.btnLostUserContent);
+
+        description.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://ml-papers.gitlab.io/android.connectivity-2017/online-appendix/antipatterns.html?id=LUC");
+                ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if(networkInfo != null){
+                    if(networkInfo.isAvailable() && networkInfo.isConnected()){
+                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                        startActivity(intent);
+                    }
+                }else if(wifiManager.isWifiEnabled() && wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLING){
+                    Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"No internet connection. Make sure Wi-Fi or cellular data is turned on, then try again.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnLostUserContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(lostContentDetail.getVisibility() == View.VISIBLE){
+                    lostContentDetail.setVisibility(View.GONE);
+                }else{
+                    lostContentDetail.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btnLostUserExample.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://ml-papers.gitlab.io/android.connectivity-2017/online-appendix/antipatternExample.html?pID=LUC-NA");
+                ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if(networkInfo != null){
+                    if(networkInfo.isAvailable() && networkInfo.isConnected()){
+                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                        startActivity(intent);
+                    }
+                }else if(wifiManager.isWifiEnabled() && wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLING){
+                    Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"No internet connection. Make sure Wi-Fi or cellular data is turned on, then try again.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
