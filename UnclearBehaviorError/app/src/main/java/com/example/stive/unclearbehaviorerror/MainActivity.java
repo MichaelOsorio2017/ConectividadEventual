c0d9901d91ef78690434ed0cf8b0be0bb9cf1e1b
MichaelOsorio2017 <ms.osorio@uniandes.edu.co>
2018-01-07 21:54:20 -0500

    Unexpected Result From Background Process

--- UnclearBehaviorError/app/src/main/java/com/example/stive/unclearbehaviorerror/MainActivity.java
+++ UnclearBehaviorError/app/src/main/java/com/example/stive/unclearbehaviorerror/MainActivity.java
@@ -150,6 +150,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        
+
     }
 }
---
+++ unexpectedResultFromBackgroundProcess/app/src/main/java/com/example/stive/unexpectedresultfrombackgroundprocess/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.stive.unexpectedresultfrombackgroundprocess;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView description;
+
+    RelativeLayout unexpectedResultDetail;
+
+    Button btnUnexpectedResult;
+    Button btnExampleUnexpectedResult;
+    Button btnTestUnexpectedResult;
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        description = findViewById(R.id.antipattern);
+
+        unexpectedResultDetail = findViewById(R.id.unexpectedResultDetail);
+        btnUnexpectedResult = findViewById(R.id.btnUnexpectedResult);
+        btnExampleUnexpectedResult = findViewById(R.id.btnExampleUnexpectedResult);
+        btnTestUnexpectedResult = findViewById(R.id.btnTestUnexpectedResult);
+
+        description.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://ml-papers.gitlab.io/android.connectivity-2017/online-appendix/antipatterns.html?id=URBP");
+                ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if(networkInfo != null){
+                    if(networkInfo.isAvailable() && networkInfo.isConnected()){
+                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                        startActivity(intent);
+                    }
+                }else if(wifiManager.isWifiEnabled() && wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLING){
+                    Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"No internet connection. Make sure Wi-Fi or cellular data is turned on, then try again.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnUnexpectedResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(unexpectedResultDetail.getVisibility() == View.VISIBLE){
+                    unexpectedResultDetail.setVisibility(View.GONE);
+                }else{
+                    unexpectedResultDetail.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btnExampleUnexpectedResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://ml-papers.gitlab.io/android.connectivity-2017/online-appendix/antipatternExample.html?pID=URBP-NA");
+                ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if(networkInfo != null){
+                    if(networkInfo.isAvailable() && networkInfo.isConnected()){
+                        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                        startActivity(intent);
+                    }
+                }else if(wifiManager.isWifiEnabled() && wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLING){
+                    Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"No internet connection. Make sure Wi-Fi or cellular data is turned on, then try again.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
